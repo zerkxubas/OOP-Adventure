e80900b9ce6ff6086f9e73f870b6a476efd95b58
Subash Sharma <zerkxubas@gmail.com>
2023-12-24 13:11:08 +0545

    initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Creating a simple Text based game by Using OOP concepts in C#.
+
+using System;
+using OOPAdventure;
+
+namespace TextGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Asking Player Name
+            Console.WriteLine("Hello Player, What Is Your Name ? ");
+            var playerName = Console.ReadLine();
+            if(playerName == string.Empty){
+                playerName = "No Name";
+            }
+            Console.WriteLine($"Welcome, {playerName} to OOP Adventure.");
+            Console.ReadKey(); 
+                        
+        }
+    }
+}
